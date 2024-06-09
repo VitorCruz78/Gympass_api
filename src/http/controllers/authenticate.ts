c7468bdf32ac69a4c8811b0b46bc816516f1b6ef
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-06-09 13:53:21 -0300

    aplicando jwt

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -16,9 +16,19 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     try {
         const authenticateUseCase = makeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({
+        const { user } = await authenticateUseCase.execute({
             email,
             password
+        })
+
+        const token = await reply.jwtSign({}, {
+            sign: {
+                sub: user.id
+            }
+        })
+
+        return reply.status(200).send({
+            token
         })
     } catch (err) {
         if (err instanceof InvalidCredentialsError) {
@@ -27,6 +37,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
         throw err
     }
-
-    return reply.status(200).send()
 }
